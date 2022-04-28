467ec34044577586761b0682611064848d6a54fb
Roberto Montalti <roberto.montalti00@gmail.com>
2022-04-28 23:22:10 +0200

    Fixed mesh generation, partially fixed uv mapping

--- Source/ProceduralPrimitives/UVSphereProcedural.cpp
+++ Source/ProceduralPrimitives/UVSphereProcedural.cpp
@@ -60,70 +60,89 @@ void AUVSphereProcedural::_emptyBuffers()
     _triangles.Empty();
 }
 
-void AUVSphereProcedural::_makeSphereNormals()
+TArray<FVector> AUVSphereProcedural::_makeSphereNormals()
 {
+    TArray<FVector> normals;
+
     for (const auto& vertex : _vertices)
     {
         auto normal = vertex - FVector::ZeroVector;
         normal.Normalize();
-        _normals.Add(normal);
+        normals.Add(normal);
     }
+
+    return std::move(normals);
 }
 
 TArray<FVector> AUVSphereProcedural::_makeSphereAxisPoints(float angleStep)
 {
     TArray<FVector> centers;
-    centers.Reserve(_meridians);
+    centers.Reserve(_parallels);
+    int32 parallels = _parallels;
 
-    for (float currentAngle = -PI; currentAngle < PI; currentAngle += angleStep)
+    for (int32 i = 0; i < _parallels; ++i)
     {
+        float currentAngle = i * angleStep;
         float unitHeight = FMath::Cos(currentAngle);
-        centers.Add(FVector(0.0f, 0.0f, (unitHeight * _radius)));
+        centers.Add(FVector(0.0f, 0.0f, - (unitHeight * _radius)));
     }
-
-    centers.Add(FVector(0.0f, 0.0f, -_radius));
 
     return std::move(centers);
 }
 
-TArray<FVector2D> _makeSphereUVs()
+TArray<FVector2D> AUVSphereProcedural::_makeSphereUVs()
 {
     TArray<FVector2D> uvs;
+    uvs.Reserve(_parallels * _meridians);
+
+    float xStep = 1.0f / (_parallels - 1);
+    float yStep = 1.0f / (_meridians - 1);
+
+    for (int32 mi = 0; mi < _meridians; mi++)
+    {
+        float y = mi * yStep;
+        for (int32 pi = 0; pi < _parallels; pi++)
+        {
+            float x = pi * xStep;
+            uvs.Add(FVector2D(x, y));
+        }
+    }
+
     return std::move(uvs);
 }
 
 void AUVSphereProcedural::_buildMesh()
 {
-    int32 pointsPerCircle = _parallels;
-    int32 nVertices = _meridians * pointsPerCircle;
-
-    float meridianAngleStep = PI / _meridians;
+    int32 nVertices = _meridians * _parallels;
+    float parallelsAngleStep = PI / (_parallels - 1);
 
     _emptyBuffers();
     _allocateBuffers(nVertices);
 
     TArray<int32> ids;
     ids.Reserve(nVertices);
-    for (int32 id = 0; id < nVertices; id++) ids.Add(id);
 
-    for (const auto& center : _makeSphereAxisPoints(meridianAngleStep))
+    for (const auto& center : _makeSphereAxisPoints(parallelsAngleStep))
     {
         float localRadius = FMath::Sqrt((_radius * _radius) - (FMath::Abs(center.Z) * FMath::Abs(center.Z)));
-        UE_LOG(LogTemp, Warning, TEXT("radius: %f with center: %f"), localRadius, center.Z);
-        _vertices.Append(CircularNGonPoints(pointsPerCircle, localRadius, center));
+        _vertices.Append(CircularNGonPoints(_meridians, localRadius, center));
     }
 
-    for (int32 circleStartingId = nVertices - pointsPerCircle;
-            circleStartingId - pointsPerCircle >= 0;
-            circleStartingId -= pointsPerCircle)
+    for (int32 id = 0; id < _vertices.Num(); id++) ids.Add(id);
+
+    for (int32 circleStartingId = _vertices.Num() - _meridians;
+            circleStartingId - _meridians >= 0;
+            circleStartingId -= _meridians)
     {
-        int32 bottomCircleStartingId = circleStartingId - pointsPerCircle;
-        auto top = TArrayView<int32>(ids).Slice(circleStartingId, pointsPerCircle);
-        auto bottom = TArrayView<int32>(ids).Slice(bottomCircleStartingId, pointsPerCircle);
+        int32 bottomCircleStartingId = circleStartingId - _meridians;
+        auto top = TArrayView<int32>(ids).Slice(circleStartingId, _meridians);
+        auto bottom = TArrayView<int32>(ids).Slice(bottomCircleStartingId, _meridians);
+
         _triangles.Append(StripFromNGonCouple(top, bottom));
     }
 
-    _makeSphereNormals();
+    _normals.Append(_makeSphereNormals());
+    _uv.Append(_makeSphereUVs());
 
     _proceduralMesh->CreateMeshSection(0, _vertices, _triangles, _normals, _uv, {}, {}, false);
 }
--- Source/ProceduralPrimitives/UVSphereProcedural.h
+++ Source/ProceduralPrimitives/UVSphereProcedural.h
@@ -20,9 +20,10 @@ private:
 
     void _buildMesh();
 
+    TArray<FVector2D> _makeSphereUVs();
+    TArray<FVector> _makeSphereNormals();
     TArray<FVector> _makeSphereAxisPoints(float heightStep);
     void _allocateBuffers(int32 nVertices);
-    void _makeSphereNormals();
     void _emptyBuffers();
 
 public:	
@@ -30,15 +31,19 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 	UPROPERTY(EditAnywhere, Category="Mesh")
-		UProceduralMeshComponent* _proceduralMesh;
+	UProceduralMeshComponent* _proceduralMesh;
+
 	UPROPERTY(EditAnywhere, Category="Material")
-		UMaterialInterface* _material;
+	UMaterialInterface* _material;
+
 	UPROPERTY(EditAnywhere, meta = (ClampMin = "1", ClampMax = "1000", UIMin = "1", UIMax = "1000"))
-		float _radius = 200;
+	float _radius = 200;
+
 	UPROPERTY(EditAnywhere, meta = (ClampMin = "3", ClampMax = "100", UIMin = "3", UIMax = "100"))
-		int32 _meridians = 30;
+	int32 _meridians = 30;
+
 	UPROPERTY(EditAnywhere, meta = (ClampMin = "3", ClampMax = "100", UIMin = "3", UIMax = "100"))
-		int32 _parallels = 30;
+	int32 _parallels = 30;
 
 protected:
 	virtual void BeginPlay() override;
